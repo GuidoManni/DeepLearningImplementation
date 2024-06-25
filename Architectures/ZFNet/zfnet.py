ac53b2b021f01b8a313b1f5353e97663e8c08f6a
gvide <guidomanni@gmail.com>
2024-06-25 15:57:21 +0200

    first commit

---
+++ Architectures/ZFNet/zfnet.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+
+class ZFNet(nn.Module):
+    def __init__(self):
+        super(ZFNet, self).__init__()
+        # First Convolutional Layer
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7, stride=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        # Second Convolutional Layer
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        # Third Convolutional Layer
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+        )
+        # Fourth Convolutional Layer
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+        )
+        # Fifth Convolutional Layer
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+
+        # Dense Layer
+        self.output = nn.Sequential(
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 1000)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        print(x.shape)
+        x = torch.reshape(x, (x.shape[0], 256 * 6 * 6))  # reshaping the tensor for the dense layer (we are flattening)
+        x = self.output(x)
+        return x
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = ZFNet().to(device)
+    x = torch.randn(1, 3, 224, 224, device=device)
+    summary(model, (3, 224, 224))
+    print(model(x).shape)
